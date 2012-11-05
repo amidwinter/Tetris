9d2721fe97662c1287a6bcd69121dc9b118397dd
Andrew Midwinter <akmidwinter@gmail.com>
2012-11-04 17:26:02 -0700

    Tetris.java: First commit.

---
+++ Tetris.java
@@ -0,0 +1,27 @@
+
+public class Tetris {
+	private String clientToken;
+	
+	public static void main(String[] args) {
+		//create pub/sub connection
+		
+		//create req/resp connection
+		
+		//send connect message over req/resp channel
+		
+		//receive and store client token
+		
+		//monitor pub/sub waiting for game to start
+		
+		//loop until game ended
+		while(true) {
+			if(newMessage==true) {
+				//read state channel information (pub/sub)
+				
+				//determine move
+				
+				//create and send move message
+			}
+		}
+	}
+}
