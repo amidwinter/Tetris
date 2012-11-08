31be42a71eb9fd72c301803cac2a456ed8f484e3
Mark Mullen <markmullen9@hotmail.com>
2012-11-07 20:28:48 -0700

    Updated connection information in Tetris.java: Password/Username seems to be incorrect

--- Tetris.java
+++ Tetris.java
@@ -5,27 +5,30 @@ public class Tetris {
 	
 	public static void main(String[] args) {
 		//create pub/sub connection
-		String pubSubServer = "ec2-54-242-48-216.compute-1.amazonaws.com";
+		String pubSubServer = "tcp://ec2-54-242-48-216.compute-1.amazonaws.com:5557";
 		String streamName = "";
 		ZMQ.Context pubSubContext = ZMQ.context(1);
 		ZMQ.Socket pubSubSocket = pubSubContext.socket(ZMQ.SUB);
 		
 		pubSubSocket.connect(pubSubServer);
-		pubSubSocket.subscribe(streamName.getBytes());
+		pubSubSocket.subscribe("B".getBytes());
 		//create req/resp connection
-		String reqRespServer = "ec2-54-242-48-216.compute-1.amazonaws.com";
+		String reqRespServer = "tcp://ec2-54-242-48-216.compute-1.amazonaws.com:5557";
 		ZMQ.Context reqRespContext = ZMQ.context(1);
 		ZMQ.Socket reqRespSocket = reqRespContext.socket(ZMQ.REQ);
 		
 		reqRespSocket.connect(reqRespServer);
 		
 		//generate connect message
-		String matchToken = "400bca5a-6283-4355-a57a-507238d21479";
-		String matchConnectMessage = "{\"comm_type\" : \"MatchConnect\", \"match_token\" : \"" + matchToken + "\", \"team_name\" : \"team70\",\"password\" : \"catz12345\"}";
+		String matchToken = "ae63b992-9d28-4ccf-98d4-a1d37cb95595";
+		String matchConnectMessage = "{\"comm_type\" : \"MatchConnect\", \"match_token\" : \"" + matchToken + "\", \"team_name\" : \"Just Two Guys\",\"password\" : \"catz54321\"}";
 		
 		//send connect message over req/resp channel
 		byte[] connectRequest = matchConnectMessage.getBytes();
 		reqRespSocket.send(connectRequest, 0);
+		byte[] reply = reqRespSocket.recv(0);
+        String replyValue = new String(reply);
+        System.out.println("Received reply: " + replyValue );
 		
 		//receive and store client token
 		
