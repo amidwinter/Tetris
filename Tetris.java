dcdaedb33cd6d8efdc8effdaea3cdb737578dd82
Mark Mullen <markmullen9@hotmail.com>
2012-11-07 18:35:46 -0700

    Updated Tetris.java with connection info

--- Tetris.java
+++ Tetris.java
@@ -1,51 +1,51 @@
-import zmq
-import time
-    context = zmq.context;
+import org.zeromq.ZMQ;
 
 public class Tetris {
 	private String clientToken;
 	
 	public static void main(String[] args) {
 		//create pub/sub connection
-		String pubSubServer = "";
-		ZMQ.Context pubSubContext = ZMQ.Context(1);
-		ZMQ.Socket pubSubSocket = ZMQ.Socket(1);
+		String pubSubServer = "ec2-54-242-48-216.compute-1.amazonaws.com";
+		String streamName = "";
+		ZMQ.Context pubSubContext = ZMQ.context(1);
+		ZMQ.Socket pubSubSocket = pubSubContext.socket(ZMQ.SUB);
 		
 		pubSubSocket.connect(pubSubServer);
-		
+		pubSubSocket.subscribe(streamName.getBytes());
 		//create req/resp connection
-		String reqRespServer = "";
-		ZMQ.Context reqRespContext = ZMQ.Context(1);
-		ZMQ.Socket reqRespSocket = ZMQ.Socket(1);
+		String reqRespServer = "ec2-54-242-48-216.compute-1.amazonaws.com";
+		ZMQ.Context reqRespContext = ZMQ.context(1);
+		ZMQ.Socket reqRespSocket = reqRespContext.socket(ZMQ.REQ);
 		
 		reqRespSocket.connect(reqRespServer);
 		
 		//generate connect message
-		String matchToken = args[0];
-		matchConnectMessage = "{
-				\"comm_type\" : \"MatchConnect\", 
-				\"match_token\" : \"" + matchToken + "\", 
-				\"team_name\" : \"team70\",
-				\"password\" : \"catz12345\"" +
-						"}";
+		String matchToken = "400bca5a-6283-4355-a57a-507238d21479";
+		String matchConnectMessage = "{\"comm_type\" : \"MatchConnect\", \"match_token\" : \"" + matchToken + "\", \"team_name\" : \"team70\",\"password\" : \"catz12345\"}";
 		
 		//send connect message over req/resp channel
 		byte[] connectRequest = matchConnectMessage.getBytes();
-		reqRespSocket.sent(connectRequest, 0);
+		reqRespSocket.send(connectRequest, 0);
 		
 		//receive and store client token
 		
 		//monitor pub/sub waiting for game to start
-		
+		while (true) {
+		      // Read envelope with address
+		      String address = new String(pubSubSocket.recv(0));
+		      // Read message contents
+		      String contents = new String(pubSubSocket.recv(0));
+		      System.out.println(address + " : " + contents);
+		    }
 		//loop until game ended
-		while(true) {
-			if(newMessage==true) {
-				//read state channel information (pub/sub)
-				
-				//determine move
-				
-				//create and send move message
-			}
-		}
+//		while(true) {
+//			if(newMessage==true) {
+//				//read state channel information (pub/sub)
+//				
+//				//determine move
+//				
+//				//create and send move message
+//			}
+//		}
 	}
 }
