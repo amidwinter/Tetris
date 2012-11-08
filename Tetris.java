214ecf57b04325c221b1ef9c78fb753a42f31edd
Andrew Midwinter <akmidwinter@gmail.com>
2012-11-07 17:57:47 -0700

    Added initial zeroMQ stuff up to sending connect message. Not tested.

--- Tetris.java
+++ Tetris.java
@@ -1,13 +1,37 @@
+import zmq
+import time
+    context = zmq.context;
 
 public class Tetris {
 	private String clientToken;
 	
 	public static void main(String[] args) {
 		//create pub/sub connection
+		String pubSubServer = "";
+		ZMQ.Context pubSubContext = ZMQ.Context(1);
+		ZMQ.Socket pubSubSocket = ZMQ.Socket(1);
+		
+		pubSubSocket.connect(pubSubServer);
 		
 		//create req/resp connection
+		String reqRespServer = "";
+		ZMQ.Context reqRespContext = ZMQ.Context(1);
+		ZMQ.Socket reqRespSocket = ZMQ.Socket(1);
+		
+		reqRespSocket.connect(reqRespServer);
+		
+		//generate connect message
+		String matchToken = args[0];
+		matchConnectMessage = "{
+				\"comm_type\" : \"MatchConnect\", 
+				\"match_token\" : \"" + matchToken + "\", 
+				\"team_name\" : \"team70\",
+				\"password\" : \"catz12345\"" +
+						"}";
 		
 		//send connect message over req/resp channel
+		byte[] connectRequest = matchConnectMessage.getBytes();
+		reqRespSocket.sent(connectRequest, 0);
 		
 		//receive and store client token
 		
