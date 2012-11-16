b75427c04c1082e48c8c1d03fcdfcb680dfd184a
Mark Mullen <markmullen9@hotmail.com>
2012-11-15 23:52:22 -0700

    Changed parser, uses new JSON parse. Still cant parse nested classes, will be able to soon

--- Parser.java
+++ Parser.java
@@ -1,10 +1,80 @@
+import java.lang.reflect.Field;
 import java.util.Map;
 import java.util.HashMap;
 
+import com.google.gson.Gson;
+
+
 public class Parser
 {
-	//Class Variables
-	Map<String,String> msg=new HashMap<String, String>();
+	private Map<String,String> msg=new HashMap<String, String>();
+	
+	public class MatchConnect
+	{
+		String comm_type;
+		String resp;
+		String client_token;
+	}
+	
+	public class GameMove
+	{
+		String comm_type;
+		String resp;
+	}
+	
+	public class GameBoardState
+	{
+		String comm_type;
+		String game_name;
+		String sequence;
+		float timestamp;
+		public class states
+		{
+			public class client1
+			{
+				String board_state;
+				int piece_number;
+				int[] cleared_rows;
+			}
+			public class client2
+			{
+				String board_state;
+				int piece_number;
+				int[] cleared_rows;
+			}
+		}
+		String[] queue;
+	}
+	
+	public class GamePieceState
+	{
+		String comm_type;
+		String game_name;
+		String sequence;
+		float timestamp;
+		public class states
+		{
+			public class client1
+			{
+				int orient;
+				String piece;
+				int number;
+				int row;
+				int col;
+			}
+			public class client2
+			{
+				int orient;
+				String piece;
+				int number;
+				int row;
+				int col;
+			}
+		}
+		String[] queue;
+	}
+	
+	
 	
 	/**
 	 * Initializes the Parser object. 
@@ -22,26 +92,94 @@ public class Parser
 	 */
 	public boolean parseMessage(String JSON)
 	{	
+		Gson gson = new Gson();
+		Field[] fields = null;
 		boolean success = true;
-		msg.clear();
-		JSON = JSON.replaceAll("\\s","");
-		String tokens[] = JSON.split(",");
-		for(int i = 0; i < tokens.length; i++)
+		// Deciding what type of message this is
+		String commType = getCommType(JSON);
+		if(commType.equals("MatchConnectResp"))
 		{
-			tokens[i] = tokens[i].replaceAll("\"", "");
-			tokens[i] = tokens[i].replace("{", "");
-			tokens[i] = tokens[i].replace("}", "");
-			String objectPair[] = tokens[i].split(":");
-			msg.put(objectPair[0], objectPair[1]);
-			
-			System.out.println(tokens[i]);
+			MatchConnect MC = gson.fromJson(JSON, MatchConnect.class);
+			fields = MatchConnect.class.getDeclaredFields();
+			msg = convertToMap(fields, MC);
 		}
+		else if(commType.equals("GameMoveResp"))
+		{
+			GameMove GM = gson.fromJson(JSON, GameMove.class);
+			fields = GameMove.class.getDeclaredFields();
+			msg = convertToMap(fields, GM);
+		}
+		else if(commType.equals("GameBoardState"))
+		{
+			GameBoardState GBS = gson.fromJson(JSON, GameBoardState.class);
+			fields = GameBoardState.states.client1.class.getDeclaredFields();
+			msg = convertToMap(fields, GBS);
+		}
+		else if(commType.equals("GamePieceState"))
+		{
+			GamePieceState GPS = gson.fromJson(JSON, GamePieceState.class);
+			fields = GamePieceState.states.client1.class.getDeclaredFields();
+			msg = convertToMap(fields, GPS);
+		}
+		else
+			success = false;
+		
 		
 		return success;
+	}
+	
+	public Map<String, String> convertToMap(Field[] fields, Object o)
+	{
+		Map<String,String> tempMap = new HashMap<String, String>();
+		try
+		{
+			for(int i = 0; fields[i] != null; i++)
+			{
+				
+				System.out.println(fields[i].getName() + " " + (String)fields[i].get(o) + " " + i);
+				
+				String name = fields[i].getName();
+				String value = (String)fields[i].get(o);
+				tempMap.put(name, value);
+			}
+		}
+		catch(IllegalAccessException e)
+		{
+			System.out.println(e);
+		}
+		catch(ArrayIndexOutOfBoundsException ai)
+		{
+			
+		}
+		return tempMap;
+	}
+	
+	public String getCommType(String JSON)
+	{
+		String messageType = JSON.replaceAll("\\s", "");
+		String tokens[] = messageType.split(",");
+		tokens[0] = tokens[0].replaceAll("\"", "");
+		tokens[0] = tokens[0].replace("{", "");
+		tokens[0] = tokens[0].replace("}", "");
+		String commType[] = tokens[0].split(":");
+		return commType[1];
 	}
 	
 	public Map<String,String> getMessage()
 	{
 		return msg;
+	}
+	
+	/**
+	 * Takes in a map containing information to be formatted into JSON, and a string telling what
+	 * sort of message it is
+	 * @param toSend A map containing the variables to be formatted
+	 * @param type A string that identifies the type of message
+	 * @return JSON The formatted string
+	 */
+	public String formatMessage(Map<String, String> toSend, String type)
+	{
+		String JSON = "";
+		return JSON;
 	}
 }
--- ParserTest.java
+++ ParserTest.java
@@ -20,27 +20,39 @@ public class ParserTest {
 	}
 	
 	@Test
-	public void testConnectMessage() {
+	public void testGetConnectMessage() {
 		String connectMessage = "{\"comm_type\" : \"MatchConnectResp\",\"resp\" : \"ok\",\"client_token\" : \"dc8d75a6-448d-4108-8bfa-52470b97f35f\"}";
 		if(!testParser.parseMessage(connectMessage))
 			fail("Parsing failed");
 
 		msg = testParser.getMessage();
-		System.out.println(msg.get("comm_type"));
 		assertEquals("MatchConnectResp", msg.get("comm_type"));
 		assertEquals("ok", msg.get("resp"));
 		assertEquals("dc8d75a6-448d-4108-8bfa-52470b97f35f", msg.get("client_token"));
 	}
 	
 	@Test
-	public void testGameMoveRespMessage(){
+	public void testGetGameMoveRespMessage(){
 		String moveMessage = "{\"comm_type\" : \"GameMoveResp\",\"resp\" : \"ok\"}";
 		if(!testParser.parseMessage(moveMessage))
 			fail("Parsing failed");
 
 		msg = testParser.getMessage();
-		System.out.println(msg.get("comm_type"));
 		assertEquals("GameMoveResp", msg.get("comm_type"));
 		assertEquals("ok", msg.get("resp"));
 	}
+	
+	@Test
+	public void testGetGameBoardStateMessage(){
+		String boardMessage = "{\"comm_type\" : \"GameBoardState\",\"game_name\" : \"My New Game\",\"sequence\" : \"1\",\"timestamp\" : 1349298294.465858,\"states\":{\"client1\": {\"board_state\" :\"00000000000000000000000000000000000000000000000000\",\"piece_number\" : 3,\"cleared_rows\" : [7,8,9]},\"client2\": {\"board_state\" :\"00000000000000000000000000000000000000000000000000\",\"piece_number\" : 4,\"cleared_rows\" : []}}}";
+		if(!testParser.parseMessage(boardMessage))
+			fail("Parsing failed");
+
+		msg = testParser.getMessage();
+		assertEquals("GameMoveResp", msg.get("comm_type"));
+		assertEquals("ok", msg.get("resp"));
+		
+	}
+	
+	
 }
