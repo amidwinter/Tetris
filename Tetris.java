7d1d5ab775a34e029794dc16b77b4aa261351a14
Andrew Midwinter <akmidwinter@gmail.com>
2012-11-16 10:59:33 -0700

    commented out all use of Parser

--- Tetris.java
+++ Tetris.java
@@ -74,24 +74,24 @@ public class Tetris {
 			String matchTokenMessage = new String(pubSubSocket.recv(0));
 
 			//read envelope with message
-			String pubSubMessage = new String(pubSubSocket.recv(0));
-			System.out.println("PubSub: " + pubSubMessage);		      
-			boolean success = parser.parseMessage(pubSubMessage);
-			Map <String, String> pubSubMessageMap = parser.getMessage();
+//			String pubSubMessage = new String(pubSubSocket.recv(0));
+//			System.out.println("PubSub: " + pubSubMessage);		      
+//			boolean success = parser.parseMessage(pubSubMessage);
+//			Map <String, String> pubSubMessageMap = parser.getMessage();
 
 			//if comm type is GameBoardState, set new values for board state and check if changes have happened
 			//else if comm type is GamePieceState, set new values for piece state and check if changes have happened
 			String pubSubCommType = pubSubMessageMap.get("comm_type");
 			if(pubSubCommType.equals("GameBoardState")) {
-				int boardState = Integer.parseInt(pubSubMessageMap.get("board_state"));
-				int pieceNumber = Integer.parseInt(pubSubMessageMap.get("piece_number"));
-				String clearedRows = pubSubMessageMap.get("cleared_rows");
+//				int boardState = Integer.parseInt(pubSubMessageMap.get("board_state"));
+//				int pieceNumber = Integer.parseInt(pubSubMessageMap.get("piece_number"));
+//				String clearedRows = pubSubMessageMap.get("cleared_rows");
 
 				//if game board has changed, kill movePicker thread (if one exists) and create a new one. 
 				//Also, set new values for board
-				if(currentBoard.hasChanged(boardState, pieceNumber)) {
+//				if(currentBoard.hasChanged(boardState, pieceNumber)) {
 					//set new values for board
-					currentBoard.setBoard(boardState, pieceNumber, clearedRows);
+//					currentBoard.setBoard(boardState, pieceNumber, clearedRows);
 					
 					//if movePicker thread exists, kill it
 					if(movePicker != null && movePicker.isAlive()) {
@@ -104,17 +104,17 @@ public class Tetris {
 			}
 			//else if comm type is GamePieceState, set new values for piece state and check if changes have happened
 			else if(pubSubCommType.equals("GamePieceState")) {
-				int orientation = Integer.parseInt(pubSubMessageMap.get("orientation"));
-				String piece = pubSubMessageMap.get("piece");
-				int number = Integer.parseInt(pubSubMessageMap.get("number"));
-				int row = Integer.parseInt(pubSubMessageMap.get("row"));
-				int col = Integer.parseInt(pubSubMessageMap.get("col"));
-
-				//if piece has changed, kill movePicker thread (if one exists) and create new one.
-				//Also, set new values for piece
-				if(currentPiece.hasChanged(orientation, piece, number, row, col)) {
-					//set new values for piece
-					currentPiece.setPiece(orientation, piece, number, row, col);
+//				int orientation = Integer.parseInt(pubSubMessageMap.get("orientation"));
+//				String piece = pubSubMessageMap.get("piece");
+//				int number = Integer.parseInt(pubSubMessageMap.get("number"));
+//				int row = Integer.parseInt(pubSubMessageMap.get("row"));
+//				int col = Integer.parseInt(pubSubMessageMap.get("col"));
+//
+//				//if piece has changed, kill movePicker thread (if one exists) and create new one.
+//				//Also, set new values for piece
+//				if(currentPiece.hasChanged(orientation, piece, number, row, col)) {
+//					//set new values for piece
+//					currentPiece.setPiece(orientation, piece, number, row, col);
 					
 					//if movePicker thread exists, kill it
 					if(movePicker != null && movePicker.isAlive()) {
