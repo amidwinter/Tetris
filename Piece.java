a9c0918663d51bdc3dbe9e8e9889b3588912795e
Andrew Midwinter <akmidwinter@gmail.com>
2012-11-16 22:48:20 -0700

    Piece: fixed array indices

--- Piece.java
+++ Piece.java
@@ -52,9 +52,9 @@ public class Piece {
 	
 	public int[][] getMask()
 	{
-		int[][] pieceMask = new int[3][3];
-		for(int i = 0; i < 3; i++)
-			for(int j = 0; j < 3; j++)
+		int[][] pieceMask = new int[4][4];
+		for(int i = 0; i < 4; i++)
+			for(int j = 0; j < 4; j++)
 				pieceMask[i][j] = 0;
 		
 		if(this.piece.equals("O"))
